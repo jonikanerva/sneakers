90cbf848a13fee2f3fda3a79005b606bb491b95c
Joni Kanerva <jhonny@jhonny.org>
2019-02-02 13:56:40 +0200

    add healh controller test

---
+++ test/getHealth.ts
@@ -0,0 +1,13 @@
+import 'jest'
+import * as supertest from 'supertest'
+import { app } from '../app/app'
+
+describe('health endpoint', () => {
+  it('returns 200', () =>
+    supertest(app)
+      .get('/health')
+      .then(response => {
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual({ status: 'ok' })
+      }))
+})
