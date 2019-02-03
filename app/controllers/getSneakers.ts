565d0916c2896c98fea570b00d081596131960a4
Joni Kanerva <jhonny@jhonny.org>
2019-02-03 14:10:02 +0200

    extract fetching and rendering

--- app/controllers/getSneakers.ts
+++ app/controllers/getSneakers.ts
@@ -1,26 +1,15 @@
 import * as R from 'ramda'
 import { NextFunction, Request, Response } from 'express'
-import { getFeedBin } from '../modules/feedbinApi'
-import { parseFeed } from '../modules/parseFeeds'
-import { createHtml } from '../modules/createHtml'
-import { cache } from '../modules/cache'
-
-const cacheOneHour = cache(3600)
-const fetchAndRender = (brand: string) => () =>
-  getFeedBin(brand)
-    .then(parseFeed)
-    .then(createHtml)
+import { renderFeed } from '../modules/renderFeed'
 
 export const getSneakers = (
   req: Request,
   res: Response,
   next: NextFunction
 ): void => {
-  const b = R.pathOr('', ['query', 'b'], req)
-  const cacheKey = b === 'adidas' ? 'adidas' : 'nike'
+  const brand = R.pathOr('', ['query', 'b'], req)
 
-  cacheOneHour
-    .readOrElse(cacheKey, fetchAndRender(b))
+  renderFeed(brand)
     .then(html => res.send(html))
     .catch(next)
 }
