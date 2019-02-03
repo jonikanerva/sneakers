ad6bbef8e81ffec87bac56d57357d3ac18e02e62
Joni Kanerva <jhonny@jhonny.org>
2019-02-03 10:26:00 +0200

    first draft of the html

--- app/modules/createHtml.ts
+++ app/modules/createHtml.ts
@@ -1,8 +1,48 @@
 import * as R from 'ramda'
 import { FeedResponse } from '../modules/parseFeeds'
 
-export const createHtml = (feed: FeedResponse[]): string =>
+const drawItem = (item: FeedResponse): string => `
+<div class='tile'>
+  <a href="${item.url}">
+    <img alt="${item.title}" alt="${item.title}" src="${item.image}"/>
+  </a>
+</div>
+`
+
+export const drawItems = (feed: FeedResponse[]): string =>
   R.compose(
     R.join(''),
-    R.map((item: FeedResponse) => `<img src="${item.image}"/>`)
+    R.map(drawItem)
   )(feed)
+
+export const createHtml = (feed: FeedResponse[]): string => `
+<html>
+<title>Sneakers</title>
+
+<style>
+.content {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%
+}
+.tile {
+  width: 350px;
+  height: 250px;
+  overflow: hidden;
+  flex-grow: 1;
+}
+.tile img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  overflow: hidden;
+}
+</style>
+
+<body>
+  <div class='content'>
+    ${drawItems(feed)}
+  </div>
+</body>
+</html>
+`
