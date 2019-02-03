0623cb6784de8eab113e3ef249a23331afedcd3f
Joni Kanerva <jhonny@jhonny.org>
2019-02-03 13:40:43 +0200

    cache html for one hour

--- app/controllers/getSneakers.ts
+++ app/controllers/getSneakers.ts
@@ -3,17 +3,24 @@ import { NextFunction, Request, Response } from 'express'
 import { getFeedBin } from '../modules/feedbinApi'
 import { parseFeed } from '../modules/parseFeeds'
 import { createHtml } from '../modules/createHtml'
+import { cache } from '../modules/cache'
+
+const cacheOneHour = cache(3600)
+const fetchAndRender = (brand: string) => () =>
+  getFeedBin(brand)
+    .then(parseFeed)
+    .then(createHtml)
 
 export const getSneakers = (
   req: Request,
   res: Response,
   next: NextFunction
 ): void => {
-  const brand = R.pathOr('', ['query', 'b'], req)
+  const b = R.pathOr('', ['query', 'b'], req)
+  const cacheKey = b === 'adidas' ? 'adidas' : 'nike'
 
-  getFeedBin(brand)
-    .then(parseFeed)
-    .then(createHtml)
+  cacheOneHour
+    .readOrElse(cacheKey, fetchAndRender(b))
     .then(html => res.send(html))
     .catch(next)
 }
