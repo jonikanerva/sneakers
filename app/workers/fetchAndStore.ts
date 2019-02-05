f87fcff7e61dd4d6906c6aae355a803e9862c1af
Joni Kanerva <jhonny@jhonny.org>
2019-02-05 09:53:33 +0200

    refac worker

--- app/workers/fetchAndStore.ts
+++ app/workers/fetchAndStore.ts
@@ -4,13 +4,11 @@ import { logger } from '../logger'
 
 const fiveHours = 60 * 60 * 5
 
-const fetchParseAndStore = (brand: string) =>
-  fetchAndParse(brand).then(data => {
-    const cacheKey = generateCacheKey(brand)
-
-    cacheSet(cacheKey, fiveHours, data)
-    logger.info({ message: `${brand} feed fetched and stored!` })
-  })
+const fetchParseAndStore = (brand: string): Promise<any> =>
+  fetchAndParse(brand)
+    .then(data => ({ data, cacheKey: generateCacheKey(brand) }))
+    .then(({ data, cacheKey }) => cacheSet(cacheKey, fiveHours, data))
+    .then(() => logger.info({ message: `${brand} feed fetched and stored!` }))
 
 Promise.all([fetchParseAndStore('nike'), fetchParseAndStore('adidas')])
   .then(() => {
