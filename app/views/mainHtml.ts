fb2f5e817d23a7713b9b909ea468c2e984c3d88b
Joni Kanerva <jhonny@jhonny.org>
2019-02-09 11:32:21 +0200

    remove small images

--- app/views/mainHtml.ts
+++ app/views/mainHtml.ts
@@ -1,3 +1,21 @@
+const javascript = `
+document.addEventListener(
+  'DOMContentLoaded',
+  function(event) {
+    var images = window.document.getElementsByTagName('img')
+
+    for (var i = 0; i < images.length; i++) {
+      var image = images[i]
+
+      if ((image.naturalHeight && image.naturalHeight < 100) || (image.naturalWidth && image.naturalWidth < 100)) {
+        image.parentElement.parentElement.style.display = 'none'
+      }
+    }
+  },
+  { once: true }
+)
+`
+
 export const mainHtml = (styles: string, content: string) => `
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +27,9 @@ export const mainHtml = (styles: string, content: string) => `
   <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/favicon.png">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no viewport-fit=cover">
   <meta name="theme-color" content="#000000">
-
+  <script>
+    ${javascript}
+  </script>
   <style>
     body {
       font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
