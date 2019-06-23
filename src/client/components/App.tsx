79f5f67b7a1b00b966050db12a41609350aa48c2
Joni Kanerva <jhonny@jhonny.org>
2019-06-23 12:14:20 +0300

    add routes

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import * as R from 'ramda'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom'
+
+import Sneakers from './Sneakers'
+import Navigation from './Navigation'
+
 import './App.css'
 
 document.addEventListener(
@@ -23,6 +33,16 @@ document.addEventListener(
   { once: true }
 )
 
-const App: React.FC = () => <div></div>
+const App: React.FC = () => (
+  <Router>
+    <Navigation />
+    <Switch>
+      <Route path="/nike" render={() => <Sneakers brand="nike" />} />
+      <Route path="/jordan" render={() => <Sneakers brand="jordan" />} />
+      <Route path="/adidas" render={() => <Sneakers brand="adidas" />} />
+      <Route path="/" render={() => <Redirect to="/nike" />} />
+    </Switch>
+  </Router>
+)
 
 export default App
