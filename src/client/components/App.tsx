4c81d46181a89e68e478d7483dc0456549697c4a
Joni Kanerva <jhonny@jhonny.org>
2019-06-23 12:33:07 +0300

    remove small images

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,37 +1,13 @@
 import React from 'react'
-import * as R from 'ramda'
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
 } from 'react-router-dom'
-
 import Sneakers from './Sneakers'
 import Navigation from './Navigation'
-
 import './App.css'
-
-document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    const images = window.document.getElementsByTagName('img')
-
-    for (const image of images) {
-      const height: number = R.propOr(0, 'naturalHeight', image)
-      const width: number = R.propOr(0, 'naturalWidth', image)
-      const parent: HTMLElement | undefined = R.path(
-        ['parentElement', 'parentElement'],
-        image
-      )
-
-      if ((width < 100 || height < 100) && parent) {
-        parent.style.display = 'none'
-      }
-    }
-  },
-  { once: true }
-)
 
 const App: React.FC = () => (
   <Router>
--- src/client/components/Image.tsx
+++ src/client/components/Image.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import * as R from 'ramda'
 import './Image.css'
 
 interface Props {
@@ -10,12 +11,38 @@ interface Props {
   image: string
 }
 
-const Image: React.FC<Props> = ({ url, title, image }) => (
-  <div className="image--tile">
-    <a href={url}>
-      <img className="image--img" title={title} alt={title} src={image} />
-    </a>
-  </div>
-)
+const removeSmallImage = (id: string): void => {
+  const element = window.document.getElementById(id)
+  const height: number = R.propOr(0, 'naturalHeight', element)
+  const width: number = R.propOr(0, 'naturalWidth', element)
+  const parent: HTMLElement | undefined = R.path(
+    ['parentElement', 'parentElement'],
+    element
+  )
+
+  if ((width < 100 || height < 100) && parent) {
+    parent.style.display = 'none'
+  }
+}
+
+const Image: React.FC<Props> = ({ url, title, image, id }) => {
+  const imageId = String(id)
+
+  useEffect(() => removeSmallImage(imageId), [id])
+
+  return (
+    <div className="image--tile">
+      <a href={url}>
+        <img
+          id={imageId}
+          className="image--img"
+          title={title}
+          alt={title}
+          src={image}
+        />
+      </a>
+    </div>
+  )
+}
 
 export default Image
