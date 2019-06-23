ba19b83a42f163f2657069fa1eb1a7e60119d699
Joni Kanerva <jhonny@jhonny.org>
2019-06-23 12:13:26 +0300

    add getFeed endpoint

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import { getHealth } from './controllers/getHealth'
 import { getSneakers } from './controllers/getSneakers'
 import { defaultError } from './controllers/defaultError'
+import { getFeed } from './controllers/getFeed'
 
 const app: express.Application = express()
 
 app.disable('x-powered-by')
 
 app.get('/', getSneakers)
+app.get('/feed/:brand', getFeed)
 app.get('/health', getHealth)
 
 app.use(express.static('build/public'))
